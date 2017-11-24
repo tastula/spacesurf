e4184dd2dd285010a0f9b02ece708da7cafd5298
Valtteri Tastula <tastula@protonmail.ch>
2017-11-24 17:43:32 +0200

    Game object is now created in main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,11 @@
+#include "game.hh"
 #include "resources.hh"
 #include <SDL2/SDL.h>
 
 int main()
 {
     Resources res;
+    Game game(res);
 
     // Run the game
     while(res.game_running)
@@ -14,6 +16,7 @@ int main()
         while(SDL_PollEvent(&res.event))
         {
             if(res.event.type == SDL_QUIT) return 0;
+            game.input();
         }
 
         // Present all changes
